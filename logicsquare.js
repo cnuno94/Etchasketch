ad421acb72749b8cc1e5d187ce458b1aea1015b8
cnuno94 <carlosnunos94@gmail.com>
2024-11-30 15:18:27 -0800

    Added button to prompt user to choose number of squares

--- logicsquare.js
+++ logicsquare.js
@@ -1,21 +1,41 @@
-const div_1 = document.querySelector("div");
-div_1.style.width = "256px";
-div_1.style.margin = "100px auto";
+const div_0 = document.querySelector("div");
+const div_1 = document.querySelector(".squares");
+let grid_width = 500;
+let num_squares;
+
+div_0.style.textAlign = "center";
+
+div_1.style.width = grid_width + "px";
+div_1.style.margin = "20px auto";
 div_1.style.display = "flex";
 div_1.style.flexWrap = "wrap";
 
-for (i = 0; i<16; i++){
-    let divs = document.createElement("div");
-    divs.style.width = "50px";
-    divs.style.height = "50px";
-    divs.style.border = "3px solid black";
-    divs.style.margin = "0.5px"
-    div_1.appendChild(divs);
-    divs.addEventListener("mouseover", () => {
-        divs.style.backgroundColor = "red";
-    })
-    divs.addEventListener("mouseout", () => {
-        divs.style.backgroundColor = "white";
-    })
-}
+const button = document.createElement("button");
+button.textContent = "Num of squares";
+button.style.padding = "10px";
+button.style.margin = "10px";
+div_0.appendChild(button);
+button.addEventListener("click", ()=>{
+    num_squares = prompt("Number of squares per side");
+    num_squares = parseInt(num_squares);
+    if (div_1.hasChildNodes()) {
+        // The div has at least one child node
+        div_1.innerHTML="";}
+    for (i = 0; i<(num_squares*num_squares); i++){
+        let divs = document.createElement("div");
+        divs.style.width = ((grid_width/num_squares)-4) + "px";
+        divs.style.height = ((grid_width/num_squares)-4) + "px";
+        divs.style.border = "1px solid black";
+        divs.style.margin = "1px"
+        div_1.appendChild(divs);
+        divs.addEventListener("mouseover", () => {
+            divs.style.backgroundColor = "red";
+        })
+        divs.addEventListener("mouseout", () => {
+            divs.style.backgroundColor = "white";
+        })
+    }
+})
+div_0.appendChild(button);
+
 
